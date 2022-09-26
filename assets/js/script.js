59e336114c65054766d5524cef2c8f70d1a94fe0
heanware <heanware@gmail.com>
2022-09-26 18:51:16 +0500

    add animated numbers healthcare__facts 1920 (+easynumberanimate.js)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,17 @@
 const videoBeforeAnimation = 300,
     $window = $(window);
 
-let windowWidth = $window.outerWidth();
+let windowWidth = $window.outerWidth(),
+    numbersAnimated = false;
 
 $(function () {
     let $videoWrappers = $(".js-wider"),
-        scrollBarWidth = window.innerWidth - $window.width();
+        scrollBarWidth = window.innerWidth - $window.width(),
+        $numbers = $(".js-number"),
+        $factsBottomBorder = $(".healthcare__facts").offset().top - 500;
+
+    console.log($factsBottomBorder);
+
     $window.on("scroll", function () {
         let scroll = $(this).scrollTop();
 
@@ -20,5 +26,31 @@ $(function () {
                 $video.css("max-width", "calc(100vw - 120px)");
             }
         });
+        if (scroll >= $factsBottomBorder && !numbersAnimated) {
+            animateNumbers();
+        }
     });
-});
+
+    function animateNumbers() {
+        numbersAnimated = true;
+        $numbers.each(function () {
+            let $this = $(this);
+            $this.easy_number_animate({
+                start_value: 0,
+                end_value: $this.data("number"),
+                duration: 800,
+                delimiter: '',
+                after: function () {
+                    let text;
+                    if ($this.data("after")) {
+                        text = $this.text() + $this.data("after");
+                    } else if ($this.data("before")) {
+                        text = $this.data("before") + $this.text();
+                    }
+                    $this.text(text);
+                }
+            });
+        });
+    }
+})
+;
